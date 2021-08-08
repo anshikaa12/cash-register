4fe598c406981a0c480517fcaebd584120ddb73f
Anshika Agarwal <agarwal.anshika100@gmail.com>
2021-08-09 01:18:19 +0530

    fixed-bugs

--- app.js
+++ app.js
@@ -11,8 +11,12 @@ hide(nextdiv);
 hide(cashmsg);
 hide(tt);
 nxtbtn.addEventListener("click", function checkBill() {
-    if (inputamt.value > 0) {
+    if (Number(inputamt.value) > 0) {
         hide(emsg);
+        for (var i = 0; i < 7; i++) {
+            var req = document.querySelector(".row" + i);
+            req.innerText = "";
+        }
         show(nextdiv, "flex");
     } else {
         errormessage(emsg, "Please enter correct Bill amount");
@@ -22,19 +26,19 @@ nxtbtn.addEventListener("click", function checkBill() {
 })
 
 check.addEventListener("click", function checkit() {
-    if (outputamt.value < inputamt.value) {
+    if (Number(outputamt.value) < Number(inputamt.value)) {
         errormessage(cashmsg, "Do you want to wash plates?");
         show(cashmsg, "block");
         hide(tt);
 
-    } else if (outputamt.value === inputamt.value) {
+    } else if (Number(outputamt.value) === Number(inputamt.value)) {
         errormessage(cashmsg, "Great! you paid the exact amount");
         show(cashmsg, "block");
         hide(tt);
 
     } else {
 
-        var amount = outputamt.value - inputamt.value;
+        var amount = Number(outputamt.value) - Number(inputamt.value);
         returnAmount(amount);
 
     }
@@ -44,25 +48,20 @@ check.addEventListener("click", function checkit() {
 const bills = [2000, 500, 100, 20, 10, 5, 1];
 function returnAmount(amount) {
 
-    if (amount < 0) {
-        errormessage(cashmsg, "Do you want to wash plates?");
-        show(cashmsg, "block");
-        hide(tt);
 
-    } else {
-        for (var i = 0; i < bills.length; i++) {
-            var c = Math.trunc(amount / bills[i]);
+    for (var i = 0; i < bills.length; i++) {
+        var c = Math.trunc(amount / bills[i]);
 
-            if (c > 0) {
-                var req = document.querySelector(".row" + i);
+        if (c > 0) {
+            var req = document.querySelector(".row" + i);
 
-                req.innerText = c;
-            }
-            amount = amount % bills[i];
+            req.innerText = c;
         }
-        show(tt, "block");
-        hide(cashmsg);
+        amount = amount % bills[i];
     }
+    show(tt, "block");
+    hide(cashmsg);
+
 
 }
 
